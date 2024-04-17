931df3407d702c53e555a9cf3f5eaa00971636df
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-04-17 22:45:11 +0200

    add app colors

--- app/src/main/java/dev/clementfloret/kanatorii/ui/theme/Color.kt
+++ app/src/main/java/dev/clementfloret/kanatorii/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package dev.clementfloret.kanatorii.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Orange = Color(0xFFFF9400)
+val OrangeLight = Color(0xFFE16914)
+val OrangeDark = Color(0xFFC05A16)
+val Green = Color(0xFF4F7144)
+val GreenLight = Color(0xFF669258)
+val GreenDark = Color(0xFF3C5533)
+val Grey = Color(0xFFC9C9C9)
+val Shadow = Color(0x33000000)
+val ShadowTopHome = Color(0xA6424041)
+val Transparent = Color.Transparent
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val GreenAnswer = Color(0xFF00E800)
+val GreenAnswerDark = Color(0xFF35AE00)
+
+val RedAnswer = Color(0xFFFF0000)
+val RedAnswerDark = Color(0xFFDA000C)
